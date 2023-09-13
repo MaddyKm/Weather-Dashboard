ea5750108747e40be4bcf28d56fccc2d77914665
MaddyKm <134648462+MaddyKm@users.noreply.github.com>
2023-09-13 09:48:45 -0400

    adds weather card styling and buttons of recent search history

--- assets/script.js
+++ assets/script.js
@@ -3,15 +3,89 @@ var APIkey = "6cf092f23b87fdf33fc57108faf70e1a";
 var cityInput = document.querySelector("#cityinput");
 var search = document.querySelector("#searchbtn");
 var formInput = document.querySelector("#inputForm");
-var weatherDisplay = document.querySelector("#weatherDisplay");
+var main = document.querySelector("#main");
+var aside = document.querySelector("#aside");
+var weatherDisplay = document.createElement("div");
+var buttons = document.querySelectorAll(".pastLocation");
 //DATA~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 //FUNCTIONS~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 function savecity() {
   console.log("button clicked!");
   console.log(cityInput.value);
+  localStorage.setItem("CityName", cityInput.value);
+  weatherDisplay.innerHTML = "";
+  weatherDisplay.remove();
   getWeather();
 }
+
+function pastSearchList() {
+  ListItem = localStorage.getItem("CityName");
+  var pastList = document.createElement("ul");
+  aside.appendChild(pastList);
+  var ListItem = document.createElement("button");
+  ListItem.setAttribute("class", "pastLocation");
+  ListItem.textContent = cityInput.value;
+  pastList.appendChild(ListItem);
+}
+
+function clickPastSearch() {
+  console.log("getting last city");
+  weatherDisplay.innerHTML = "";
+  var currentCity = buttons.textContent;
+  var apiUrl =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    currentCity +
+    "&appid=" +
+    APIkey +
+    "&units=imperial";
+
+  fetch(apiUrl)
+    .then(function (response) {
+      if (response.ok) {
+        console.log(response);
+        response.json().then(function (data) {
+          console.log(data);
+
+          weatherDisplay.setAttribute("id", "weatherDisplay");
+          main.appendChild(weatherDisplay);
+          var iconDisplay = document.createElement("img");
+
+          var icons = data.weather[0].icon;
+          iconDisplay.src =
+            "http://openweathermap.org/img/wn/" + icons + "@2x.png";
+
+          weatherDisplay.appendChild(iconDisplay);
+          var cityName = document.createElement("p");
+
+          cityName.innerText = cityInput.value;
+
+          weatherDisplay.appendChild(cityName);
+
+          var temp = document.createElement("p");
+          temp.innerText = "Temp: " + data.main.temp;
+
+          weatherDisplay.appendChild(temp);
+
+          var wind = document.createElement("p");
+          wind.innerText = "Wind: " + data.wind.speed + " MPH";
+
+          weatherDisplay.appendChild(wind);
+
+          var humidity = document.createElement("p");
+          humidity.innerText = "Humidity: " + data.main.humidity + "%";
+
+          weatherDisplay.appendChild(humidity);
+        });
+      } else {
+        alert("Error " + response.statusText);
+      }
+    })
+    .catch(function (error) {
+      alert("Unable to connect to weather");
+    });
+}
+
 function getWeather() {
   var apiUrl =
     "https://api.openweathermap.org/data/2.5/weather?q=" +
@@ -26,6 +100,16 @@ function getWeather() {
         console.log(response);
         response.json().then(function (data) {
           console.log(data);
+
+          weatherDisplay.setAttribute("id", "weatherDisplay");
+          main.appendChild(weatherDisplay);
+          var iconDisplay = document.createElement("img");
+
+          var icons = data.weather[0].icon;
+          iconDisplay.src =
+            "http://openweathermap.org/img/wn/" + icons + "@2x.png";
+
+          weatherDisplay.appendChild(iconDisplay);
           var cityName = document.createElement("p");
 
           cityName.innerText = cityInput.value;
@@ -35,17 +119,17 @@ function getWeather() {
           var temp = document.createElement("p");
           temp.innerText = "Temp: " + data.main.temp;
 
-          cityName.appendChild(temp);
+          weatherDisplay.appendChild(temp);
 
           var wind = document.createElement("p");
           wind.innerText = "Wind: " + data.wind.speed + " MPH";
 
-          temp.appendChild(wind);
+          weatherDisplay.appendChild(wind);
 
           var humidity = document.createElement("p");
           humidity.innerText = "Humidity: " + data.main.humidity + "%";
 
-          wind.appendChild(humidity);
+          weatherDisplay.appendChild(humidity);
         });
       } else {
         alert("Error " + response.statusText);
@@ -76,5 +160,10 @@ function getWeather() {
 search.addEventListener("click", function (event) {
   event.preventDefault();
   savecity();
+  pastSearchList();
 });
+
+for (var i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", clickPastSearch(), false);
+}
 //INITIALIZATIONS~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
